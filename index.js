1a9efefffc15976dbf1835c52c10904d4f753083
Andrea T <tostonar@gmail.com>
2019-10-13 19:14:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  let filtered = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+  return filtered;
+}
+
+function fuzzyMatch(drivers, string) {
+  let filtered = drivers.filter(driver=> driver.startsWith(string));
+  return filtered;
+}
+
+function matchName(drivers, string) {
+  let filtered = drivers.filter(driver => driver.name === string);
+  return filtered;
+}
