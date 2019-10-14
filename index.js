7ceccfe60ee3748e60ef49077e5c1b7dcd63dbea
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-14 10:37:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(arry, str) {
+    //returns the matching list of drivers. The function should be case insensitive
+    return arry.filter(driver => driver.toUpperCase() === str.toUpperCase())
+}
+
+function fuzzyMatch(arry, str) {
+    //returns all drivers whose names begin with the provided letters
+    return arry.filter(driver => driver.startsWith(str))
+}
+
+function matchName(arry, str) {
+    //returns each element whose name property matches the provided string argument.
+    return arry.filter(driver => driver.name === str)
+}
